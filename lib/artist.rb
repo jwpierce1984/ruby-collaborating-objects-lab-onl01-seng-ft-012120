77c19128fb71cee539a90fd33de0898b0087daa0
Justin Pierce <jwpierce1984@gmail.com>
2020-02-04 18:20:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,15 @@
 class Artist
-  attr_accessor :name, :songs
-  
-  def initialize (name)
+  attr_accessor :name
+ 
+  def initialize(name)
     @name = name
-    @songs = [] 
-    save
   end
-  
-  def self.all
-    @@all
-  end
-  
+ 
   def add_song(song)
-    @songs << song
     song.artist = self
   end
-  
-  def save
-    @@all << self
-  end
-  
+ 
   def songs
     Song.all.select {|song| song.artist == self}
   end
-  
 end
