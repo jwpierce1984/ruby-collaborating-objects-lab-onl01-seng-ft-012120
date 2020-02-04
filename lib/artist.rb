7aa180db3647e9be08db299789414583131d3ae5
Justin Pierce <jwpierce1984@gmail.com>
2020-02-04 18:08:19 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+  
+  def initialize (name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+  attr_accessor :title, :artist
+  @@all = []
+  
+  def initialize (title)
+    @title = title
+    @artist = artist
+  end
+  
+#  def artist_name = name
+#    self.artist = Artist.name
+#    artist.add_song(self)  
+#  end
+  
+  
+  
+end
