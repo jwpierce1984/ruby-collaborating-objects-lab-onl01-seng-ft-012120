0ef0e6f2c954d275acc88b8ce7645fca0d6358dc
Justin Pierce <jwpierce1984@gmail.com>
2020-02-04 19:54:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,8 +17,7 @@ class Song
   end
   
   def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
+    artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
     new_song.save
